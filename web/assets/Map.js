3f1da7dad80488f240b9bc6a4dd7eb7fa2e9cd6a
SylvioT <sylvio.tavares@hotmail.com>
2017-06-21 22:48:43 -0300

    Added background map

--- web/assets/Map.js
+++ web/assets/Map.js
@@ -42,26 +42,50 @@ function Map(id) {
         { 'x': 315, 'y': 35, 'material': 'tile_0' },
         { 'x': 315, 'y': 70, 'material': 'tile_0' },
       ],
+      'backgrounds': [
+        {'id': "bg_0", 'sprite': "bg_0.png", 'width': 700, 'height': 200, 'z': -2},
+        {'id': "bg_1", 'sprite': "bg_1.png", 'width': 700, 'height': 200, 'z': -1},
+      ],
+      'npcs': [],
     }
 
-    that.build(data.materials, data.tiles, data.collisions);
+    that.build(data.materials, data.tiles, data.collisions, data.backgrounds);
   }
 
   this.addPlayer = function(player) {}
   this.addEnemy = function(enemy) {}
   this.addNPC = function(NPC) {}
-  this.addBackground = function(background) {}
+  this.addBackground = function(background) {
+    var texture = new THREE.ImageUtils.loadTexture('resources/backgrounds/' + background.sprite),
+        material = new THREE.MeshBasicMaterial({map: texture, transparent: true}),
+        mesh = new THREE.Mesh(new THREE.PlaneGeometry(background.width, background.height), material);
 
-  this.build = function(materials, tiles, collisions) {
+    mesh.position.set(0, background.height/2, background.z);
+
+    that.group.add(mesh); 
+  }
+
+  this.addTile = function(tile) {
+      var texture = that.materials[tile.material],
+          material  = new THREE.MeshBasicMaterial({map: texture}),
+          mesh = new THREE.Mesh(new THREE.PlaneGeometry(35, 35), material);
+
+      mesh.position.set(tile.x, tile.y, 0);
+
+      that.group.add(mesh);
+  }
+
+  this.build = function(materials, tiles, collisions, backgrounds) {
     for(var material of materials) {
       that.materials[material.id] = new THREE.ImageUtils.loadTexture('resources/tiles/' + material.sprite);
     }
     
     for(var tile of tiles) {
-      var material = new THREE.MeshBasicMaterial({map: that.materials[tile.material]});
-      var mesh = new THREE.Mesh(new THREE.PlaneGeometry(35, 35), material);
-      mesh.position.set(tile.x, tile.y, 0);
-      that.group.add(mesh);
+      that.addTile(tile);
+    }
+
+    for(var background of backgrounds) {
+      that.addBackground(background);
     }
   }
 
