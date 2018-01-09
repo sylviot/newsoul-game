9ff621cc0cd3ecef67f0e1bdeb7bacc2e8cd42d6
SylvioT <sylvio.tavares@hotmail.com>
2018-01-08 20:23:20 -0400

    Using Typescript with Three, RequireJS and more...

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,25 @@
+// concat = require('gulp-concat'),
+// livereload = require('gulp-livereload'),
+// minify = require('gulp-minify'),
+// uglify = require('gulp-uglify'),
+// rename = require('gulp-rename'),
+// debug = require('gulp-debug');
+
 var gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    livereload = require('gulp-livereload'),
-    minify = require('gulp-minify'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    debug = require('gulp-debug');
+    ts = require('gulp-typescript');
 
-var files = [
-  './web/assets/BugReport.js',
-  './web/assets/Camera.js',
-  './web/assets/Chat.js',
-  './web/assets/Control.js',
-  './web/assets/Map.js',
-  './web/assets/Player.js',
-  './web/assets/Text.js',
-  './web/assets/Scenes/*.js',
-  './web/assets/Network.js',
-];
-
-gulp.task('prod', function(){
-  var filesProduction = files;
-  filesProduction.push('./web/assets/Logic.js');
-
-  return gulp.src(filesProduction)
-  .pipe(concat('game.js'))
-  .pipe(minify({ext:{min:'.min.js'}}))
-  .pipe(gulp.dest('./web/'))
-});
-
-gulp.task('dev', function(){
-  var filesDev = [
-    './web/assets/Map.js',
-    './web/assets/Player.js'
-  ];
-
-  return gulp.src(filesDev)
-  .pipe(concat('game-dev.js'))
-  .pipe(minify({ext:{min:'.min.js'}}))
-  .pipe(gulp.dest('./web/'));
-});
-
-gulp.task('prod-watch', function(){
-  livereload.listen();
-  gulp.watch(['./web/assets/*.js', './web/assets/*/*.js'], ['prod-watch']);
-});
-
-gulp.task('dev-watch', function(){
-  livereload.listen();
-  gulp.watch(['./web/assets/*.js', './web/assets/*/*.js'], ['dev']);
+gulp.task('scripts', function(){
+  return gulp.src('./web/components/Main.ts')
+    .pipe(ts({
+      module: 'amd',
+      outFile: 'game.js'
+    }))
+    .pipe(gulp.dest('./web'));
 })
 
-gulp.task('default', ['dev', 'prod']);
+gulp.task('watch', function(){
+  // livereload.listen()
+  return gulp.watch('./web/components/*', ['scripts'])
+})
+
+gulp.task('default', ['watch', 'scripts'])
