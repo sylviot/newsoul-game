e40502c9a107966df327204ca959fa31aac4405c
SylvioT <sylvio.tavares@hotmail.com>
2018-01-25 23:56:31 -0400

    Change function anonymous to arrow in Control

--- web/components/Control.ts
+++ web/components/Control.ts
@@ -6,19 +6,17 @@ export class Control {
   constructor(_scene: IScene) {
     this._hotkeys = new Array<string>()
     
-    let that = this
-
-    document.body.onkeydown = function(_event){ 
-      let hotkey = that._hotkeys[_event.keyCode]
+    document.body.onkeydown = (_event) => { 
+      let hotkey = this._hotkeys[_event.keyCode]
       
-      if(hotkey)
+      if (hotkey) {
         _scene._keyboadEvent(hotkey)
+      }
     }
-    
-    
-    // document.body.onmouseover = function(_event){ that.mouseEvent(_event) })
-    document.body.oncontextmenu = function(_event) { _event.preventDefault(); _scene._mouseEvent('RIGHT CLICK') }
-    document.body.onclick = function(_event){ _event.preventDefault(); _scene._mouseEvent('LEFT CLICK') }
+
+    document.body.onmousemove = (_event) => { _scene._mouseEvent({name: 'MOVE', x: _event.clientX, y: _event.clientY}) }
+    document.body.oncontextmenu = (_event) => { _event.preventDefault(); _scene._mouseEvent('RIGHT CLICK') }
+    document.body.onclick = (_event) => { _event.preventDefault(); _scene._mouseEvent('LEFT CLICK') }
   }
 
   loadDefaultHotkeys(_data: any) {
