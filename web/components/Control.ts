a6589ca2cb53d29ef5a3c181d502b53c58a41c48
SylvioT <sylvio.tavares@hotmail.com>
2018-02-01 01:08:45 -0400

    Method default hotkeys

--- web/components/Control.ts
+++ web/components/Control.ts
@@ -19,17 +19,31 @@ export class Control {
     document.body.onmousemove = (_event) => { _scene._mouseEvent({name: 'MOVE', x: _event.clientX, y: _event.clientY}) }
     document.body.oncontextmenu = (_event) => { _event.preventDefault(); _scene._mouseEvent('RIGHT CLICK') }
     document.body.onclick = (_event) => { _event.preventDefault(); _scene._mouseEvent('LEFT CLICK') }
+
+    this.defaultHotkeys()
+  }
+
+  customHotkeys(_hotkeys): void {
+    _hotkeys.forEach(hk => {
+      this._hotkeys[hk.action] = hk.code
+    });
+  }
+
+  defaultHotkeys(): void {
+    var hotkeys = [
+      { action:'LEFT', code: 65},
+      { action:'DOWN', code: 83},
+      { action:'RIGHT', code: 68},
+      { action:'UP', code: 87},
+      { action:'SPACE', code: 32}
+    ];
+
+    this.customHotkeys(hotkeys);
   }
 
   isPressed(_hotkey: string): boolean {
     let _hotkeyCode = this._hotkeys[_hotkey]
 
     return ( _hotkeyCode && this._keys[_hotkeyCode] )
-  }
-
-  loadDefaultHotkeys(_data: any) {
-    _data.forEach(item => {
-      this._hotkeys[item.action] = item.code
-    });
   }
 }
