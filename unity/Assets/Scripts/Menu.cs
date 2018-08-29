18f58b25bc8cdeaadc52267fbe3705acb5746b71
sylviot <sylvio.tavares@hotmail.com>
2018-08-29 00:04:42 -0300

    Added Unity Project and .gitignore

---
+++ unity/Assets/Scripts/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	public void MenuOnClick() {
+		Debug.Log("Click");
+	}
+
+	public void Update() {
+		if(Input.GetKeyDown("space")) {
+			SceneManager.LoadScene("Game");
+		}
+	}
+}
---
+++ unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	public float jumpForce;
+	public float speed;
+	private float moveInput;
+
+	private bool isGrounded;
+	public Transform feetPos;
+	public float checkRadius;
+	public LayerMask whatIsGround;
+
+	void Start () {
+			rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveInput = Input.GetAxisRaw("Horizontal");
+		rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+		
+		isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+		if ( isGrounded && Input.GetKeyDown(KeyCode.Space)) {
+			rb.velocity = Vector2.up * jumpForce;
+		}
+	}
+}
