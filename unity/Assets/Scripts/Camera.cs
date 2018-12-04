0ef8623a9624c959f65a628571f53d1905cbe646
sylviot <sylvio.tavares@hotmail.com>
2018-12-03 22:57:44 -0200

    Basic camera follow

---
+++ unity/Assets/Scripts/Camera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private GameObject _player;
+
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var players = GameObject.FindGameObjectsWithTag("Player");
+
+        if (players.Length == 0)
+            return;
+
+        _player = players[0];
+
+        var position = new Vector3(Mathf.Max(_player.transform.position.x, 0), Mathf.Max(_player.transform.position.y, 0), transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, position, 0.1f);
+    }
+}
---
+++ unity/Assets/Scripts/CharacterNetwork.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class CharacterNetwork : NetworkBehaviour
+{
+    /* Fields */
+    [SerializeField]
+    private float _speed = 5f;
+
+    [SerializeField]
+    private float _jumpForce = 400f;
+
+    [SerializeField]
+    private LayerMask _groundLayer;
+
+    [SerializeField]
+    private Transform _groundCheck;
+
+    [SerializeField]
+    private Transform _ceilingCheck;
+
+    [SerializeField]
+    private Camera _camera;
+
+    /* Attributes */
+    const float _groundedRadius = .2f;
+    private Rigidbody2D _rigidbody2D;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _camera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(horizontal, 0, vertical);
+            transform.position += movement * Time.deltaTime * _speed;
+
+
+            bool isGrounded = Physics2D.OverlapCircle(_groundCheck.position, _groundedRadius, _groundLayer) == null;
+
+            if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+            {
+                _rigidbody2D.velocity = Vector2.up * _jumpForce;
+            }
+        }
+    }
+}
