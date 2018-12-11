16e9e92c29d8abd98b63042236e326369d90961b
sylviot <sylvio.tavares@hotmail.com>
2018-12-10 23:48:43 -0200

    Basic splash fadein

---
+++ unity/Assets/ChangeStatus.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeStatus : MonoBehaviour
+{
+
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        AnimatorStateInfo animatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        if (animatorStateInfo.IsName("SplashFadeInFinish"))
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
