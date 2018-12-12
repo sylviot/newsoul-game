bc95940bd3c2e1a597b437e4d53dee040b6556b6
sylviot <sylvio.tavares@hotmail.com>
2018-12-12 19:56:01 -0200

    Added playground

---
+++ unity/Assets/Playground/Server.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Server : MonoBehaviour
+{
+
+    NetworkManager network;
+
+    void Start()
+    {
+        network = GetComponent<NetworkManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        network.StartHost();
+    }
+}
