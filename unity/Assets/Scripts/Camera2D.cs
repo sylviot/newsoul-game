312a8450f165630f29add836099cbb2496782863
sylviot <sylvio.tavares@hotmail.com>
2018-12-12 21:10:27 -0200

    Added nickname above player

--- unity/Assets/Scripts/Camera2D.cs
+++ unity/Assets/Scripts/Camera2D.cs
@@ -2,24 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class Camera2D : MonoBehaviour
 {
     private GameObject _player;
 
-	void Start ()
-    {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    void Update()
     {
         var players = GameObject.FindGameObjectsWithTag("Player");
 
-        if (players.Length == 0)
-            return;
+        if (players.Length > 0)
+            _player = players[0];
 
-        _player = players[0];
+        if (_player == null)
+            return;
 
         var position = new Vector3(Mathf.Max(_player.transform.position.x, 0), Mathf.Max(_player.transform.position.y, 0), transform.position.z);
         transform.position = Vector3.Lerp(transform.position, position, 0.1f);
---
+++ unity/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+
+    private Text _nickname;
+    private Camera _camera;
+
+    void Start()
+    {
+        _camera = FindObjectOfType<Camera>();
+        _nickname = GetComponentInChildren<Text>();
+
+        _nickname.text = "[DEV] sylviot";
+    }
+
+    void Update()
+    {
+        Vector3 position = RectTransformUtility.WorldToScreenPoint(_camera, this.transform.position);
+
+        _nickname.transform.position = position + new Vector3(0, 25f, 0);
+    }
+}
